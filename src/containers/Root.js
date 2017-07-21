f3d9f724c6a9d539b4443e06267ae349715ef5da
Guru Upase <gurushant.upase@synerzip.com>
2017-07-21 17:31:11 +0530

    improve code for store

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -4,8 +4,7 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import configureStore from '../store/configureStore';
-const store = configureStore();
+import store from '../store/configureStore';
 import MainComponent from '../components/MainComponent';
 
 export default class Root extends React.Component {
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,13 +6,11 @@ import thunk from 'redux-thunk';
 import {applyMiddleware, compose, createStore} from 'redux';
 import createLogger from 'redux-logger';
 
-export default function configureStore(initialStore) {
-    let createStoreWithMiddleware;
-    const logger = createLogger();
-    const middleware = applyMiddleware(thunk, logger);
-    createStoreWithMiddleware = compose(middleware);
 
-    const store = createStoreWithMiddleware(createStore)(rootReducer, initialStore);
+const logger = createLogger();
+const middleware = applyMiddleware(thunk, logger);
+let createStoreWithMiddleware = compose(middleware);
 
-    return store;
-}
+const Store = createStoreWithMiddleware(createStore)(rootReducer);
+
+export default Store;
